ca4bc75ec60123f7c9d7f8ae30d2d2d47af12ef5
naquik <nknaqui72@gmail.com>
2025-06-28 15:14:18 +0530

    complete whole userRoute

--- server/controllers/user-Conntroller.js
+++ server/controllers/user-Conntroller.js
@@ -2,14 +2,14 @@ const User = require("../models/userModel");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const crypto = require('crypto')
-const mailSender=require('../utils/mailSender')
+const crypto = require("crypto");
+const mailSender = require("../utils/mailSender");
 
 //register user
 const register = async (req, res) => {
   try {
     const { name, email, password, phoneNo } = req.body;
-    console.log(req.body)
+    console.log(req.body);
 
     // Check if user Email exists
     const existingUserEmail = await User.findOne({ email });
@@ -81,86 +81,110 @@ const register = async (req, res) => {
 };
 
 //otp verification
-const optVerification = async (req,res)=>{
+const optVerification = async (req, res) => {
   try {
-      const {otp} = req.body;
+    const { otp } = req.body;
     const id = req.params.id;
-    if(!otp){
-        return res.status(400).json({
-            success:false,
-            message:"please provide verification code"
-        })
+    if (!otp) {
+      return res.status(400).json({
+        success: false,
+        message: "please provide verification code",
+      });
     }
     const user = await User.findById(id);
-    if(!user){
-        return res.status(400).json({
-            success:false,
-            message:"user not found"
-        })
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "user not found",
+      });
     }
-    if(user.isVerified){
-        return res.status(400).json({
-            success:false,
-            message:"user already verified"
-        })
+    if (user.isVerified) {
+      return res.status(400).json({
+        success: false,
+        message: "user already verified",
+      });
     }
-    if(!user.verificationCode){
-        return res.status(400).json({
-            success:false,
-            message:"verification code not sent"
-        })
+    if (!user.verificationCode) {
+      return res.status(400).json({
+        success: false,
+        message: "verification code not sent",
+      });
     }
-    if(user.verificationCode !== otp){
-        return res.status(400).json({
-            success:false,
-            message:"invalid verification code"
-        })
+    if (user.verificationCode !== otp) {
+      return res.status(400).json({
+        success: false,
+        message: "invalid verification code",
+      });
     }
-    if(user.verificationCodeExpire < Date.now()){
-        return res.status(400).json({
-            success:false,
-            message:"verification code expired"
-        })
+    if (user.verificationCodeExpire < Date.now()) {
+      return res.status(400).json({
+        success: false,
+        message: "verification code expired",
+      });
     }
     user.isVerified = true;
-    await user.save()
+    await user.save();
 
-    const token = jwt.sign({ id: user._id , name:user.name , username:user.username , email:user.email }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    });
-    
+    const userEmail = user.email;
+    const userPhone = user.phoneNo;
+
+    const algorithm = "aes-256-cbc";
+    const key = Buffer.from(process.env.AES_KEY, "hex");
+    const iv = Buffer.from(process.env.AES_IV, "hex");
+
+
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    const encryptedEmail = cipher.update(userEmail, "utf-8", "hex");
+    encryptedEmail += cipher.final("hex");
+
+    const encryptedPhone = cipher.update(String(userPhone), "utf-8", "hex");
+    encryptedPhone += cipher.final("hex");
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        phoneNo: encryptedPhone,
+        email: encryptedEmail,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
     return res.status(200).json({
-        success:true,
-        message:"user verified",
-        data:{
-            token
-        }
-    })
+      success: true,
+      message: "user verified",
+      data: {
+        token,
+      },
+    });
   } catch (error) {
     res.status(500).json({
-      success:false,
-      message:error.message
-    })
+      success: false,
+      message: error.message,
+    });
   }
 };
 
-const forgetPasswordOTP = async (req , res) =>{
- try {
-     const {email} = req.body
+//generating otp for forget password by email
+const forgetPasswordOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
 
-  const user = User.findOne({email , isVerified:true})
+    const user = User.findOne({ email, isVerified: true });
 
-   const resetPasswordToken = Math.floor(100000 + Math.random() * 900000);
-   const resetPasswordTokenExpire = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes from now
+    const resetPasswordToken = Math.floor(100000 + Math.random() * 900000);
+    const resetPasswordTokenExpire = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes from now
 
+    user.resetPasswordToken = resetPasswordToken;
+    user.resetPasswordTokenExpire = resetPasswordTokenExpire;
 
-   user.resetPasswordToken = resetPasswordToken
-   user.resetPasswordTokenExpire = resetPasswordTokenExpire
+    await user.save();
 
-   await user.save()
-
-   mailSender({
-     email: newUser.email,
+    mailSender({
+      email: newUser.email,
       subject: "Forget Password Code",
       message: `
     <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
@@ -179,167 +203,305 @@ const forgetPasswordOTP = async (req , res) =>{
       <p>Regards,<br><strong>eKart Security Team</strong></p>
     </div>
   `,
-   })
+    });
 
-   const token = jwt.sign({id:user._id} , process.env.JWT_SECRET , {
-    expiresIn: "10min"
-   })
-
-   res.status(200).json({
-    success:true,
-    id:user._id
-   })
- } catch (error) {
-  res.status(500).json({
-      success:false,
-      message:error.message
-    })
- }
-}
-
-const forgetPassword = async (req,res)=>{
- try {
-  //taking opt from req body
-    const {otp} = req.body
-
-     const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: "Token is required",
-        });
-    }
-
-    const id = jwt.decode(token , process.env.JWT_SECRET).id
-
-    //finding user By id
-    const user = User.findById(id)
-
-   if(!user){
-    res.status(404).json({
-      success:false,
-      message:"user not found"
-    })
-
-    if(otp!==user.resetPasswordToken){
-      res.status(401).json({
-        status:false,
-        message:"invalid otp"
-      })
-    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "10min",
+    });
 
     res.status(200).json({
-      success:true,
-      message:"OTP Verified"
-    })
-   }
- } catch (error) {
-  res.status(500).json({
-      success:false,
-      message:error.message
-    })
- }
+      success: true,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-}
+//verifying otp for forget password
+const forgetPassword = async (req, res) => {
+  try {
+    //taking opt from req body
+    const { otp } = req.body;
 
-const updateForgetPassword = async (req , res) => {
-  const {password , confirmPassword} = req.body
-  
-     const token = req.headers['authorization']?.split(' ')[1];
+    const token = req.headers["authorization"]?.split(" ")[1];
     if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: "Token is required",
-        });
+      return res.status(401).json({
+        success: false,
+        message: "Token is required",
+      });
     }
 
-    const id = jwt.decode(token , process.env.JWT_SECRET).id
+    const id = jwt.decode(token, process.env.JWT_SECRET).id;
 
-  const user = User.findById(id)
+    //finding user By id
+    const user = User.findById(id);
 
-  if(password !== confirmPassword){
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
 
-    res.status(401).json({
-      success:false,
-      message:"Password and Confirm Password not match"
-    })
+      if (otp !== user.resetPasswordToken) {
+        res.status(401).json({
+          status: false,
+          message: "invalid otp",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "OTP Verified",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//updating password
+const updateForgetPassword = async (req, res) => {
+  const { password, confirmPassword } = req.body;
+
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Token is required",
+    });
   }
 
-  const hashedPassword = await bcrypt.hash(password , 10)
+  const id = jwt.decode(token, process.env.JWT_SECRET).id;
 
-  user.password = hashedPassword
+  const user = User.findById(id);
 
-  await user.save()
-}
+  if (password !== confirmPassword) {
+    res.status(401).json({
+      success: false,
+      message: "Password and Confirm Password not match",
+    });
+  }
 
-const login= async (req,res) =>{
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  user.password = hashedPassword;
+
+  await user.save();
+};
+
+//login
+const login = async (req, res) => {
   try {
-    const {email , password} = req.body
+    const { email, password } = req.body;
 
-    const user = await User.findOne({email})
-    if(!user){
-      res.status(404).json({
-        success:false,
-        message:"user not find check you email"
-      })
-    }
-    const isMatchPassword =bcrypt.compare(password , user.password)
-
-    if(!isMatchPassword){
-      res.status(401).json({
-        success:false,
-        message:"Incorrect Password check your password"
-      })
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found, check your email",
+      });
     }
 
-    const userEmail = user.email
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+    if (!isMatchPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect password, check your password",
+      });
+    }
 
-    const algorithm = 'aes-256-cbc';
+    const algorithm = "aes-256-cbc";
+    const key = Buffer.from(process.env.AES_KEY, "hex");
+    const iv = Buffer.from(process.env.AES_IV, "hex");
+
+
+    const encrypt = (text) => {
+      const cipher = crypto.createCipheriv(algorithm, key, iv);
+      let encrypted = cipher.update(text, "utf8", "hex");
+      encrypted += cipher.final("hex");
+      return encrypted;
+    };
+
+    const encryptedEmail = encrypt(user.email);
+    const encryptedPhone = encrypt(String(user.phoneNo));
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email: encryptedEmail,
+        phoneNo: encryptedPhone,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      data: token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//updatating profile
+const editProfile = async (req, res) => {
+  const { name, email, phone } = req.body;
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const userId = decoded.id;
+  const user = await Profile.findById(userId);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+  if (req.file) {
+    const { url, publicId } = await uploadImage(req.file.path);
+    user.profilePic = {
+      url,
+      publicId,
+    };
+  }
+  if (name) {
+    user.name = name;
+  }
+  if (email) {
+    user.email = email;
+  }
+  if (phone) {
+    user.phone = phone;
+  }
+  await user.save();
+  return res.status(200).json({
+    success: true,
+    message: "Profile updated successfully",
+  });
+};
+
+//changing password
+
+const changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword, confirmPassword } = req.body;
+    const email = req.userInfo.email;
+
+    const algorithm = "aes-256-cbc";
     const key = crypto.randomBytes(32); // Generate a random encryption key
     const iv = crypto.randomBytes(16); // Generate a random initialization vector
 
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encryptedEmail = cipher.update(userEmail, 'utf-8', 'hex');
-    encryptedEmail += cipher.final('hex');
-    console.log(encryptedEmail)
+    // Example of decrypting data using AES in Node.js
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let decryptedEmail = decipher.update(email, "hex", "utf-8");
+    decryptedEmail += decipher.final("utf-8");
 
-    
-// Example of decrypting data using AES in Node.js
-const decipher = crypto.createDecipheriv(algorithm, key, iv);
-let decryptedData = decipher.update(encryptedEmail, 'hex', 'utf-8');
-decryptedData += decipher.final('utf-8');
+    console.log(decryptedEmail);
 
-console.log('Decrypted data:', decryptedData);
+    const user = await User.findOne(decryptedEmail);
 
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User not find",
+      });
+    }
 
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
 
-    const token = jwt.sign({id:user._id,
-      name:user.name,
-      email:encryptedEmail
-    }, process.env.JWT_SECRET, {
-            expiresIn: "7d" 
-          })
+    if (!isMatch) {
+      res.status(401).json({
+        success: false,
+        message: "invalid password",
+      });
+    }
+    if (newPassword !== confirmPassword) {
+      res.status(401).json({
+        success: false,
+        message: "password don't match check newPassword or confirmPassword",
+      });
+    }
+
+    const hashedPassword = bcrypt.hash(newPassword, 10);
+
+    user.password = hashedPassword;
 
     res.status(200).json({
-      success:true,
-      message:"log in successfully",
-      data:token
-    })
-
+      success: true,
+      message: "password change successfully",
+    });
   } catch (error) {
     res.status(500).json({
-      success:false,
-      message:error.message
-    })
+      success: false,
+      message: error.message,
+    });
   }
-}
+};
 
+const getInfo = async (req, res) => {
+  try {
+    const userData = req.userInfo;
+
+    const encryptedEmail = userData.email;
+    const encryptedPhoneNo = userData.phoneNo;
+
+    const algorithm = "aes-256-cbc";
+    const key = Buffer.from(process.env.AES_KEY, "hex");
+    const iv = Buffer.from(process.env.AES_IV, "hex");
+// Generate a random initialization vector
+
+    const decipherEmail = crypto.createDecipheriv(algorithm, key, iv);
+let decryptedEmail = decipherEmail.update(encryptedEmail, "hex", "utf-8");
+decryptedEmail += decipherEmail.final("utf-8");
+
+const decipherPhone = crypto.createDecipheriv(algorithm, key, iv);
+let decryptedPhone = decipherPhone.update(encryptedPhoneNo, "hex", "utf-8");
+decryptedPhone += decipherPhone.final("utf-8");
+
+
+    res.status(200).json({
+      success: true,
+      data: {
+        name: userData.name,
+        email: decryptedEmail,
+        phoneNo: decryptedPhone,
+        id: userData.id,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   register,
   optVerification,
   forgetPassword,
   forgetPasswordOTP,
-  updateForgetPassword ,
-  login
+  updateForgetPassword,
+  login,
+  editProfile,
+  changePassword,
+  getInfo,
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const { register, optVerification, login, forgetPassword, forgetPasswordOTP, updateForgetPassword } = require('../controllers/user-Conntroller');
+const { register, optVerification, login, forgetPassword, forgetPasswordOTP, updateForgetPassword, editProfile, changePassword, getInfo } = require('../controllers/user-Conntroller');
+const authMiddleware = require('../middleware/auth-middleware');
 const router = express.Router();
 
 // User registration route
 router.post('/auth/register', register);
 router.post('/auth/otp/:id' , optVerification )
 router.post('/auth/login' , login )
-router.post('/auth/forgetPassword' , forgetPassword )
-router.post('/auth/forgetPasswordOTP' , forgetPasswordOTP )
-router.post('/auth/updateForgetPassword' , updateForgetPassword )
+router.post('/auth/forget-Password' , forgetPassword )
+router.post('/auth/forget-Password-OTP' , forgetPasswordOTP )
+router.post('/auth/update-Forget-Password' , updateForgetPassword )
+router.post('/auth/edit-profile' , editProfile )
+router.post('/auth/change-password' , changePassword )
+router.get('/auth/me' ,authMiddleware, getInfo )
 
 
 module.exports = router;
